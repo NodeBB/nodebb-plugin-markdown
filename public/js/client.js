2d19ba05da41ef26a3d7121168df6775e8da19b2
psychobunny <rodrigues.andrew@gmail.com>
2014-11-19 01:40:28 -0500

    remove window.markdown global var, update setTimeout for promise-like pattern, jslinted.
    oh I didn't test, did this @barisusakli promode.

--- public/js/client.js
+++ public/js/client.js
@@ -1,36 +1,46 @@
+"use strict";
+
+/*global hljs, RELATIVE_PATH*/
+
 $(document).ready(function() {
-	var Markdown = {};
+	var Markdown = {}, config, done;
 
 	Markdown.init = function() {
-		$.get(RELATIVE_PATH + '/markdown/config', function(config) {
-			window.Markdown = config;
+		$.get(RELATIVE_PATH + '/markdown/config', function(_config) {
+			config = _config;
 
 			var cssEl = document.createElement('link');
 			cssEl.rel = 'stylesheet';
 			cssEl.href = RELATIVE_PATH + '/plugins/nodebb-plugin-markdown/styles/' + config.theme;
+
 			var head = document.head || document.getElementsByTagName("head")[0];
 			if (head) {
 				head.appendChild(cssEl);
 			}
+
+			if (done) {
+				done();
+			}
 		});
 	};
 
-	Markdown.highlight = function(ev, retry) {
-		if (window.Markdown) {
-			if (window.Markdown.highlight) {
-				var codeBlocks = $('.topic-text pre code');
-
-				codeBlocks.each(function(i, block) {
-					hljs.highlightBlock(block);
-				});
-			}
-		} else if (!retry) {
-			// Try once more in one second
-			setTimeout(function() {
-				Markdown.highlight(null, true);
-			}, 1000);
+	Markdown.highlight = function(ev) {
+		if (config) {
+			highlight();
+		} else {
+			done = highlight;
 		}
 	};
+
+	function highlight() {
+		if (config.highlight) {
+			var codeBlocks = $('.topic-text pre code');
+
+			codeBlocks.each(function(i, block) {
+				hljs.highlightBlock(block);
+			});
+		}
+	}
 
 	$(window).on('action:connected', Markdown.init);
 	$(window).on('action:posts.loaded action:topic.loaded action:posts.edited', Markdown.highlight);
