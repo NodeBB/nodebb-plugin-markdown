369bb6ed4b5dad8f3f977c032546d60050063c86
Julian Lam <julian@nodebb.org>
2018-08-20 11:08:38 -0400

    one more hljs styles dir fix

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ var Markdown = {
 	},
 
 	loadThemes: function () {
-		fs.readdir(path.join(__dirname, 'node_modules/highlight.js/styles'), function (err, files) {
+		fs.readdir(path.join(__dirname, '../highlight.js/styles'), function (err, files) {
 			if (err) {
 				winston.error('[plugin/markdown] Could not load Markdown themes: ' + err.message);
 				Markdown.themes = [];
