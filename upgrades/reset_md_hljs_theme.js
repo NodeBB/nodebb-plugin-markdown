d01fe49055e4ef07479a3bc65fc3ce5bba49c557
Barış Soner Uşaklı <barisusakli@gmail.com>
2022-02-08 12:35:43 -0500

    fix: don't crash if highlightTheme is falsy

--- upgrades/reset_md_hljs_theme.js
+++ upgrades/reset_md_hljs_theme.js
@@ -8,13 +8,15 @@ module.exports = {
 	timestamp: Date.UTC(2022, 0, 31),
 	method: async () => {
 		const { highlightTheme } = await meta.settings.get('markdown');
-		await markdown.loadThemes();
+		if (highlightTheme) {
+			await markdown.loadThemes();
 
-		let newTheme = highlightTheme.replace('.css', '.min.css');
-		if (!markdown.themes.includes(newTheme)) {
-			newTheme = 'default.min.css';
+			let newTheme = highlightTheme.replace('.css', '.min.css');
+			if (!markdown.themes.includes(newTheme)) {
+				newTheme = 'default.min.css';
+			}
+
+			await meta.settings.setOne('markdown', 'highlightTheme', newTheme);
 		}
-
-		await meta.settings.setOne('markdown', 'highlightTheme', newTheme);
 	},
 };
