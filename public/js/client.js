ad900b1e3d738dc1efc7c2aa2e579c5ce347d00b
Julian Lam <julian@nodebb.org>
2023-08-21 10:18:42 -0400

    fix: move component require to top

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-	require(['markdown'], (markdown) => {
+	require(['markdown', 'components'], (markdown, components) => {
 		$(window).on('action:composer.enhanced', function (evt, data) {
 			var textareaEl = data.postContainer.find('textarea');
 			markdown.capturePaste(textareaEl);
@@ -13,10 +13,7 @@
 		}, markdown.highlight);
 
 		$(window).on('action:posts.loaded action:topic.loaded action:posts.edited', function (ev, data) {
-			require(['components'], function (components) {
-				markdown.highlight(components.get('post/content').find('pre code'));
-			});
-
+			markdown.highlight(components.get('post/content').find('pre code'));
 			markdown.enhanceCheckbox(ev, data);
 		});
 	});
